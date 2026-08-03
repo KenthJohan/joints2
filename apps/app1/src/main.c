9adaf04012a1aaf02c5e9a63d713affbbaef68bc
Johan Söderlind Åström <johan.soderlindastrom@permobil.com>
2026-08-03 15:26:53 +0200

    fix keybindings

--- apps/app1/src/main.c
+++ apps/app1/src/main.c
@@ -59,6 +59,10 @@ int main(int argc, char *argv[])
 	ecs_log_set_level(-1);
 
 	ecs_log_set_level(0);
+	ecs_script_run_file(world, "config/keybindings_common.flecs");
+	ecs_log_set_level(-1);
+
+	ecs_log_set_level(0);
 	ecs_script_run_file(world, "config/physics.flecs");
 	ecs_log_set_level(-1);
 
